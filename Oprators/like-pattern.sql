3f6680cdcc1bc1814135bd725e81689cb806d1c4
Aabid92 <shaikhaabid608@gmail.com>
2023-02-02 22:54:35 +0530

    like-and-pattern

---
+++ Oprators/like-pattern.sql
@@ -0,0 +1,15 @@
+SELECT first_name LIKE '%S%' FROM
+memberships;
+
+
+SELECT first_name 
+FROM memberships 
+WHERE first_name LIKE "%a%"
+
+SELECT first_name
+FROM memberships
+WHERE first_name LIKE 'J____';
+
+
+
+
