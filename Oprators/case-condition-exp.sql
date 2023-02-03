9318fc2cd7b5ff2fe8142364f61d4dcc5c4386ab
Aabid92 <shaikhaabid608@gmail.com>
2023-02-03 21:21:33 +0530

    Update case-condition-exp.sql

--- Oprators/case-condition-exp.sql
+++ Oprators/case-condition-exp.sql
@@ -1,6 +1,15 @@
 SELECT amount_billed,
-CASE WHEN amount_billed > 30 THEN 'Good Day'
+CASE WHEN amount_billed > 30 THEN 'Good Day'     -----order matters
      WHEN amount_billed > 15 THEN 'Normal Day'
      ELSE 'Bad Day'
 END
 FROM orders;
+
+SELECT amount_billed,
+CASE WHEN amount_billed > 15 AND amount_billed <= 30 THEN 'Normal Day'
+     WHEN amount_billed > 30 THEN 'Good Day'
+     ELSE 'Bad Day'
+END
+FROM orders;
+
+
