7cc5eb2f3dc4bfccd6077ea6fc8a2feed1114425
Aabid92 <shaikhaabid608@gmail.com>
2023-03-09 12:38:17 +0530

    Update windows-functions.sql

--- aggregate-functions/windows-functions.sql
+++ aggregate-functions/windows-functions.sql
@@ -7,6 +7,6 @@ SELECT booking_date, amount_tipped, SUM(amount_tipped)
 OVER(PARTITION BY booking_date ORDER BY amount_tipped ASC)
 FROM bookings;
 
-SELECT booking_date, amount_tipped, RANK()
+SELECT booking_date, amount_tipped
 OVER(PARTITION BY booking_date ORDER BY amount_tipped DESC)
 FROM bookings;
