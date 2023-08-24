6578a373bafd672bb1eb13b55cc287df84788b57
Aabid92 <40827170+Aabid92@users.noreply.github.com>
2023-08-24 10:50:42 +0530

    Update joins.sql

--- Joins/joins.sql
+++ Joins/joins.sql
@@ -1,45 +1,45 @@
 
--- -- CREATE TABLE users(
--- --     id INT PRIMARY KEY AUTO_INCREMENT,
--- --     first_name VARCHAR(300) NOT NULL,
--- --     last_name VARCHAR(300) NOT NULL,
--- --     email VARCHAR(300) NOT NULL,
--- --     address_id INT REFERENCES addresses (id) ON DELETE RESTRICT
--- -- );
+CREATE TABLE users(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    first_name VARCHAR(300) NOT NULL,
+    last_name VARCHAR(300) NOT NULL,
+    email VARCHAR(300) NOT NULL,
+    address_id INT REFERENCES addresses (id) ON DELETE RESTRICT
+);
 
--- -- CREATE TABLE addresses(
--- --     id INT PRIMARY KEY AUTO_INCREMENT,
--- --     street VARCHAR(300) NOT NULL,
--- --     house_number VARCHAR(50) NOT NULL,
--- --     city_id INT NOT NULL
--- -- );
+CREATE TABLE addresses(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    street VARCHAR(300) NOT NULL,
+    house_number VARCHAR(50) NOT NULL,
+    city_id INT NOT NULL
+);
 
--- -- CREATE TABLE cities(
--- --     id INT PRIMARY KEY AUTO_INCREMENT,
--- --     name VARCHAR(300) NOT NULL
+CREATE TABLE cities(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(300) NOT NULL
 
--- -- );
+);
 
--- CREATE TABLE users (
---   id INT PRIMARY KEY AUTO_INCREMENT, -- MySQL
--- --   id SERIAL PRIMARY KEY, -- Postgresql
---   first_name VARCHAR(300) NOT NULL,
---   last_name VARCHAR(300) NOT NULL,
---   email VARCHAR(300) NOT NULL,
---   address_id INT REFERENCES addresses (id) ON DELETE RESTRICT
--- );
+CREATE TABLE users (
+  id INT PRIMARY KEY AUTO_INCREMENT, -- MySQL
+--   id SERIAL PRIMARY KEY, -- Postgresql
+  first_name VARCHAR(300) NOT NULL,
+  last_name VARCHAR(300) NOT NULL,
+  email VARCHAR(300) NOT NULL,
+  address_id INT REFERENCES addresses (id) ON DELETE RESTRICT
+);
 
--- CREATE TABLE addresses (
---   id INT PRIMARY KEY AUTO_INCREMENT, -- MySQL
--- --   id SERIAL PRIMARY KEY, -- Postgresql
---   street VARCHAR(300) NOT NULL,
---   house_number VARCHAR(50) NOT NULL,
---   city_id INT NOT NULL
--- );
+CREATE TABLE addresses (
+  id INT PRIMARY KEY AUTO_INCREMENT, -- MySQL
+--   id SERIAL PRIMARY KEY, -- Postgresql
+  street VARCHAR(300) NOT NULL,
+  house_number VARCHAR(50) NOT NULL,
+  city_id INT NOT NULL
+);
 
--- CREATE TABLE cities (
---   id INT PRIMARY KEY AUTO_INCREMENT, -- MySQL
--- --   id SERIAL PRIMARY KEY, -- Postgresql
---   name VARCHAR(300) NOT NULL
--- );
+CREATE TABLE cities (
+  id INT PRIMARY KEY AUTO_INCREMENT, -- MySQL
+--   id SERIAL PRIMARY KEY, -- Postgresql
+  name VARCHAR(300) NOT NULL
+);
 
